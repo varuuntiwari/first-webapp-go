4e14c6e07232921ddbcb2a942d15e20f8137d533
Varun Tiwari <varuntiwari2001@gmail.com>
2022-01-19 02:02:37 +0530

    Basic webapp completed
    A wiki made from the basic tutorial given on Golang's website

--- wiki.go
+++ wiki.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
+	reg "regexp"
 )
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 type Page struct {
 	Title string
 	Body  []byte
 }
 
+// Regular expression to validate URL ensuring vague paths cannot be accessed on server
+var validateURL = reg.MustCompile("^/(save|edit|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	u := validateURL.FindStringSubmatch(r.URL.Path)
+	if u == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid URL")
+	}
+	return u[2], nil
+}
+
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := p.Title + ".html"
 	return os.WriteFile(filename, p.Body, 0644)
 }
 
+func renderTemplate(w http.ResponseWriter, tmp string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmp + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := title + ".html"
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -24,9 +49,48 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	val := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(val)}
+	err = p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("Hello World")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
